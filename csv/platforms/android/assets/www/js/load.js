6c13ea3da838e8798574ebc5048653e8a667f9f0
adikhadilkar <aditya@tekticks.in>
2016-05-07 18:37:50 +0530

    07-05-16

--- csv/platforms/android/assets/www/js/load.js
+++ csv/platforms/android/assets/www/js/load.js
@@ -1,18 +1,24 @@
 var files1=[];
-
+var ex;
 $("#filename").change(function(e) {
     var ext = $("input#filename").val().split(".").pop().toLowerCase();
-	alert(ext);
+	ex=$("input#filename").val().replace(/^.*[\\\/]/, '');
+	var x = document.getElementById("filename").value;
+	//alert(x);
+	alert(ex);
+	//alert(ext);
 	files1 = this.files;
-	alert(files1);
+	//alert(files1);
 
     if($.inArray(ext, ["csv"]) == -1) {
-    alert('Upload CSV');
+    myApp.alert('Upload CSV');
     return false;
     }
 
     if (e.target.files != undefined) {
-		alert(e.target.files);
+		//alert(e);
+		//alert(e.target);
+		//alert(e.target.files);
         var reader = new FileReader();
         var csvLines;
         var csvValues;
@@ -38,13 +44,49 @@ $("#filename").change(function(e) {
         };
         reader.readAsText(e.target.files.item(0));
     }
+	else
+	{
+		myApp.alert('data is selected once');
+	}
     return false;
 	
 }); 
 
- function myDelete()
+function myDelete()
 {
-	alert("myDelete called..");
+	//alert("myDelete called..");
+	//alert(ex);
+	/* window.requestFileSystem(window.TEMPORARY, 5*1024*1024, gotFS, fail); */ 
+	
+	/* $.ajax
+				({
+				url: 'getname.php',
+				type:"post",
+				data:{type:ex},
+				cache: false, 
+				 success: function(response)
+					{
+						alert("success");
+						
+					}
+				});	 */
+	
+	$.ajax({
+			type: "POST",
+			url: "http://localhost/csv/www/js/getname.php",
+			data: {data1 : ex}, 
+			cache: false,
+			success: function(){
+            alert("OK");
+							},
+							error: function()
+							{
+								alert("error");
+							}
+					}); 
+	
+	
+	/* alert("myDelete called..");
 	alert(files1[0]);
    
 		var root = getFileSystemRoot();
@@ -55,12 +97,44 @@ $("#filename").change(function(e) {
             };
             
             // retrieve a file and truncate it
-            root.getFile("input#filename", {create: false}, remove_file, onFileSystemError);
-			alert("file deleted");
-
-	
+            root.getFile(ex, {create: false}, remove_file, onFileSystemError);
+			alert("file deleted");  */
 }	
 
+	/*  function gotFS(fileSystem) 
+	{
+		alert("inside gotfs");
+        fileSystem.root.getFile(ex, {create: true, exclusive: false}, gotFileEntry, fail);
+    }
+	
+	function gotFileEntry(fileEntry) 
+	{
+		alert("inside gotfileentry");
+        fileEntry.createWriter(gotFileWriter, fail);
+    }
+	
+	function gotFileWriter(writer) 
+	{
+		alert("inside gotfilewriter");
+        writer.onwriteend = function(evt) {
+            console.log("contents of file now 'some sample text'");
+            writer.truncate(11);  
+            writer.onwriteend = function(evt) {
+                console.log("contents of file now 'some sample'");
+                writer.seek(4);
+                writer.write(" different text");
+                writer.onwriteend = function(evt){
+                    console.log("contents of file now 'some different text'");
+                }
+            };
+        };
+        writer.write("some sample text");
+    } 
+	
+	  function fail(error) {
+        console.log(error.code);
+    } 
+ */
  function onFileSystemError(error) 
  {
 	var msg = 'file system error: ' + error.code;
