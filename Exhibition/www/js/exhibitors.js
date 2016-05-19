3f906acefe8b14f5136f4bec0011ed7e300a1ad3
adikhadilkar <aditya@tekticks.in>
2016-05-19 19:24:20 +0530

    19-05-16

--- Exhibition/www/js/exhibitors.js
+++ Exhibition/www/js/exhibitors.js
@@ -50,7 +50,7 @@ var request = createCORSRequest( "post", "http://radio.tekticks.com" );
 			
 			/* $('#alphabet').append('<div class="list-block media-list" style="margin:0px 0px"><ul><li><a href="eachExhibitor.html" class="item-link item-content" id="'+id[i]+'" onclick="getEachExhibitor(this)"><div class="item-inner"><div class="item-title-row"><div class="item-title">'+companyName[i]+'</div></div></div></a></li></ul></div>'); */
 			
-			$('#alphabet').append('<div class="list-block media-list" style="margin:0px 0px" ><ul><li><a href="eachExhibitor.html" class="item-link item-content" id="'+id[i]+'" onclick="getEachExhibitor(this)"><div class="item-media"><img src="'+link[i]+'" width="100"></div><div class="item-inner"><div class="item-title-row"><div class="item-title"><b>'+companyName[i]+'</b></div><div class="item-after"></div></div><div class="item-subtitle">'+sectorName[i]+'</div><div class="item-text">'+description[i]+'</div></div></a></li></ul></div>');
+			$('#alphabet').append('<div class="list-block media-list" style="margin:0px 0px" ><ul><li><a href="exhibitorProfile2.html" class="item-link item-content" id="'+id[i]+'" onclick="getEachExhibitor(this)"><div class="item-media"><img src="'+link[i]+'" width="100"></div><div class="item-inner"><div class="item-title-row"><div class="item-title"><b>'+companyName[i]+'</b></div><div class="item-after"></div></div><div class="item-subtitle">'+sectorName[i]+'</div><div class="item-text">'+description[i]+'</div></div></a></li></ul></div>');
 			
 		}
 		}
@@ -64,6 +64,278 @@ var request = createCORSRequest( "post", "http://radio.tekticks.com" );
 }
 	)};
 }
+
+
+
+function getEachExhibitor(item)
+{
+	myApp.showPreloader();
+	var request = createCORSRequest( "post", "http://radio.tekticks.com" );
+	if(request)
+	{
+	var exhibitorId = $(item).attr("id");	
+	var data = {"exhibitor":[{"exhibitorId":exhibitorId}]};
+	localStorage.setItem("exhibitorId", exhibitorId);
+	
+	var sendData = function(data)
+	{		
+		$.ajax({
+		url:"http://radio.tekticks.com/exhibition/exhibitorDetails.php",
+		type: 'POST',
+		dataType:"json",
+		data: JSON.stringify(data),
+		contentType: 'application/json',
+		success:function(response)
+		{
+			if(JSON.stringify(response.status)==200)
+			{
+				var id= JSON.stringify(response.exhibitorsDetails[0].id).replace(/"/g,"");
+				var about= JSON.stringify(response.exhibitorsDetails[0].about).replace(/"/g,"");
+				var primEmail= JSON.stringify(response.exhibitorsDetails[0].primEmail).replace(/"/g,"");
+				var secEmail= JSON.stringify(response.exhibitorsDetails[0].secEmail).replace(/"/g,"");
+				var primContact= JSON.stringify(response.exhibitorsDetails[0].primContact).replace(/"/g,"");
+				var secContact= JSON.stringify(response.exhibitorsDetails[0].secContact).replace(/"/g,"");
+				var website= JSON.stringify(response.exhibitorsDetails[0].website).replace(/"/g,"");
+				var companyName= JSON.stringify(response.exhibitorsDetails[0].companyName).replace(/"/g,"");
+				var mobileNo= JSON.stringify(response.exhibitorsDetails[0].mobileNo).replace(/"/g,"");
+				var address1= JSON.stringify(response.exhibitorsDetails[0].address1).replace(/"/g,"");
+				var address2= JSON.stringify(response.exhibitorsDetails[0].address2).replace(/"/g,"");
+				var landmark= JSON.stringify(response.exhibitorsDetails[0].landmark).replace(/"/g,"");
+				var pincode= JSON.stringify(response.exhibitorsDetails[0].pincode).replace(/"/g,"");
+				var maplink= JSON.stringify(response.exhibitorsDetails[0].maplink).replace(/"/g,"");
+				var link= JSON.stringify(response.exhibitorsDetails[0].link).replace(/"/g,"");
+				
+				localStorage.setItem("about", about);
+				localStorage.setItem("primEmail", primEmail);
+				localStorage.setItem("secEmail", secEmail);
+				localStorage.setItem("primContact", primContact);
+				localStorage.setItem("secContact", secContact);
+				localStorage.setItem("website", website);
+				localStorage.setItem("companyName", companyName);
+				localStorage.setItem("mobileNo", mobileNo);
+				localStorage.setItem("address1", address1);
+				localStorage.setItem("address2", address2);
+				localStorage.setItem("landmark", landmark);
+				localStorage.setItem("pincode", pincode);
+				localStorage.setItem("maplink", maplink);
+				localStorage.setItem("link", link);
+				myApp.hidePreloader();
+				
+				initialize5();
+				
+			}	
+		
+		}
+		});
+}
+sendData(data);	
+console.log(data);
+}
+}
+
+
+
+ function initialize5()
+{		
+	var show = document.getElementById('exhibitorProfile');
+	//alert(show);
+    show.style.visibility = 'visible';
+	
+	var about = localStorage.getItem("about"); 
+	//alert(about);
+	var primEmail = localStorage.getItem("primEmail"); 
+	var secEmail= localStorage.getItem("secEmail");
+	var primContact = localStorage.getItem("primContact");
+	var secContact = localStorage.getItem("secContact");
+	var website = localStorage.getItem("website");
+	var companyName = localStorage.getItem("companyName");
+	var mobileNo = localStorage.getItem("mobileNo");
+	var address1 = localStorage.getItem("address1");
+	var address2 = localStorage.getItem("address2");
+	//alert(address2);
+	var landmark = localStorage.getItem("landmark");
+	var pincode = localStorage.getItem("pincode");
+	var maplink = localStorage.getItem("maplink");
+	var link = localStorage.getItem("link");
+	
+	if(about.replace(/\s/g,"") == "")
+	{
+	
+		$("#about").fadeOut();
+	}
+	else
+	{
+	    $("#about").fadeIn();
+		$("#about").text(about);
+          		  
+	}
+	
+	if(primEmail.replace(/\s/g,"") == "")
+	{
+	
+		$("#primEmail").fadeOut();
+	}
+	else
+	{
+	    $("#primEmail").fadeIn();
+		$("#primEmail").text(primEmail);
+          		  
+	}
+	
+	if(secEmail.replace(/\s/g,"") == "")
+	{
+	
+		$("#secEmail").fadeOut();
+	}
+	else
+	{
+	    $("#secEmail").fadeIn();
+		$("#secEmail").text(secEmail);
+          		  
+	}
+	
+	if(primContact.replace(/\s/g,"") == "")
+	{
+	
+		$("#primContact").fadeOut();
+	}
+	else
+	{
+	    $("#primContact").fadeIn();
+		$("#primContact").text(primContact);
+          		  
+	}
+	
+	if(secContact.replace(/\s/g,"") == "")
+	{
+	
+		$("#secContact").fadeOut();
+	}
+	else
+	{
+	    $("#secContact").fadeIn();
+		$("#secContact").text(secContact);
+          		  
+	}
+	
+	if(website.replace(/\s/g,"") == "")
+	{
+	
+		$("#website").fadeOut();
+	}
+	else
+	{
+	    $("#website").fadeIn();
+		$("#website").text(website);
+          		  
+	}
+	
+	
+	if(link.replace(/\s/g,"") == "")
+	{
+	
+		$("#link").fadeOut();
+	}
+	else
+	{
+	    $("#link").fadeIn();
+		$("#link").attr("src",link);    		  
+	}
+
+	if(maplink.replace(/\s/g,"") == "")
+	{
+	
+		$("#maplink").fadeOut();
+	}
+	else
+	{
+	    $("#maplink").fadeIn();
+		$("#maplink").attr("src",maplink);    		  
+	}
+
+	if(companyName.replace(/\s/g,"") == "")
+	{
+	
+		$("#companyName").fadeOut();
+		$("#company").fadeOut();
+	}
+	else
+	{
+	    $("#companyName").fadeIn();
+		$("#company").fadeIn();
+		$("#companyName").text(companyName);
+		$("#company").text(companyName);
+          		  
+	}
+	
+	if(mobileNo.replace(/\s/g,"") == "")
+	{
+	
+		$("#mobileNo").fadeOut();
+	}
+	else
+	{
+	    $("#mobileNo").fadeIn();
+		$("#mobileNo").text(mobileNo);
+          		  
+	}
+	
+	if(address1.replace(/\s/g,"") == "")
+	{
+	
+		$("#address1").fadeOut();
+	}
+	else
+	{
+	    $("#address1").fadeIn();
+		$("#address1").text(address1);
+          		  
+	}
+	
+	if(address2.replace(/\s/g,"") == "")
+	{
+	
+		$("#address2").fadeOut();
+		$("#address21").fadeOut();
+	}
+	else
+	{
+	    $("#address2").fadeIn();
+		$("#address21").fadeIn();
+		$("#address2").text(address2);
+		$("#address21").text(address2);
+          		  
+	}
+	
+	if(landmark.replace(/\s/g,"") == "")
+	{
+	
+		$("#landmark").fadeOut();
+	}
+	else
+	{
+	    $("#landmark").fadeIn();
+		$("#landmark").text(landmark);
+          		  
+	}
+	
+	if(pincode.replace(/\s/g,"") == "")
+	{
+	
+		$("#pincode").fadeOut();
+	}
+	else
+	{
+	    $("#pincode").fadeIn();
+		$("#pincode").text(pincode);
+          		  
+	}
+	
+	
+		
+	
+} 
+
 
 
 
@@ -110,7 +382,8 @@ var request = createCORSRequest( "post", "http://radio.tekticks.com" );
 			
 			/* $('#alphabet').append('<div class="list-block media-list" style="margin:0px 0px"><ul><li><a href="eachExhibitor.html" class="item-link item-content" id="'+id[i]+'" onclick="getEachExhibitor(this)"><div class="item-inner"><div class="item-title-row"><div class="item-title">'+companyName[i]+'</div></div></div></a></li></ul></div>'); */
 			
-			$('#sector').append('<div class="list-block accordion-list" style="margin:0px 0px" id="ml"><ul><li class="accordion-item"><a href="#" class="item-content item-link" id="'+id[i]+'" onclick="eachSector(this)" data-popup=".popup-about" class="open-popup"><div class="item-inner"><div class="item-title" style="word-wrap:break-word">'+sectorName[i]+'</div></div></a></li></ul></div>');
+			 $('#sector').append('<div class="list-block accordion-list" style="margin:0px 0px" id="ml"><ul><li class="accordion-item"><a href="#" class="item-content item-link open-popup" data-popup=".demo-rateus" id="'+id[i]+'" onclick="eachSector(this)"><div class="item-inner"><div class="item-title" style="word-wrap:break-word">'+sectorName[i]+'</div></div></a></li></ul></div>'); 
+			
 		}
 		}
 		else
@@ -126,7 +399,7 @@ var request = createCORSRequest( "post", "http://radio.tekticks.com" );
 
 function eachSector(item)
 {
-//myApp.showPreloader();
+myApp.showPreloader();
 var request = createCORSRequest( "post", "http://radio.tekticks.com" );
 	if(request)
 	{
@@ -146,8 +419,8 @@ var request = createCORSRequest( "post", "http://radio.tekticks.com" );
 		{
 			if(JSON.stringify(response.status)==200)
 			{
-				//myApp.hidePreloader();
-				
+				myApp.hidePreloader();
+				$("#sectorContent").find("div").remove();
 				var id=[];
 				var companyName=[];
 				var logolink=[];
@@ -159,25 +432,13 @@ var request = createCORSRequest( "post", "http://radio.tekticks.com" );
 					logolink[i]= JSON.stringify(response.secExhibitorInformation[i].link).replace(/"/g,"");
 				}
 				
-				//alert(id);
-				//alert(companyName);
-				//alert(logolink);
+
 				
-				
-				
-				
-				
-				/* for(var j=0;j<response.secExhibitorInformation.length;j++)
+				 for(var j=0;j<response.secExhibitorInformation.length;j++)
 				{ 
-				/* $('#sector').append('<div class="list-block accordion-list" style="margin:0px 0px" id="ml"><ul><li class="accordion-item"><a href="#" class="item-content item-link" id="'+id[i]+'" onclick="eachSector(this)"><div class="item-inner"><div class="item-title" style="word-wrap:break-word">'+sectorName[i]+'</div></div></a></li></ul></div>'); 
-				
-				$('#sector').append('<div class="popover popover-links"><div class="popover-angle"></div><div class="popover-inner"><div class="list-block"><ul><li><a href="#" class="list-button item-link">Link 1</a></li><li><a href="#" class="list-button item-link">Link 2</a></li><li><a href="#" class="list-button item-link">Link 3</a></li><li><a href="#" class="list-button item-link">Link 4</a></li></ul></div></div></div>');
-				
-				
-				
-				
-				
-				} */
+
+				$('#sectorContent').append('<div class="list-block media-list" style="margin:0px 0px" ><ul><li><a href="exhibitorProfile2.html" class="item-link item-content" id="'+id[j]+'" onclick="getEachExhibitor(this)"><div class="item-media"><img src="'+logolink[j]+'" width="100"></div><div class="item-inner"><div class="item-title-row"><div class="item-title"><b>'+companyName[j]+'</b></div><div class="item-after"></div></div><div class="item-text"></div></div></a></li></ul></div>');
+				} 
 				
 			}	
 		
@@ -190,39 +451,3 @@ console.log(data);
 }
 
 
-function getEachExhibitor(item)
-{
-	var request = createCORSRequest( "post", "http://radio.tekticks.com" );
-	if(request)
-	{
-	var sectorId = $(item).attr("id");	
-	var data = {"sectors":[{"sectorId":sectorId}]};
-	localStorage.setItem("sectorId", sectorId);
-	
-	var sendData = function(data)
-	{		
-		$.ajax({
-		url:"http://radio.tekticks.com/exhibition/sectorExhibitors.php",
-		type: 'POST',
-		dataType:"json",
-		data: JSON.stringify(data),
-		contentType: 'application/json',
-		success:function(response)
-		{
-			if(JSON.stringify(response.status)==200)
-			{
-				
-				
-				
-				
-				
-				
-			}	
-		
-		}
-		});
-}
-sendData(data);	
-console.log(data);
-}
-}
--- Exhibition/www/server/exhibitorDetails.php
+++ Exhibition/www/server/exhibitorDetails.php
@@ -8,35 +8,39 @@ header('Access-Control-Allow-Headers: Content-Type');
 include 'jsonFormat.php';
 include 'jsonDeliver.php';
 include 'config.php';
-$data=array();
+$json = file_get_contents("php://input");
 $data1=array();
+$data = json_decode($json, true);
+$selectedExhibitors=array();
+$jsonresponse=array();
+$exhibitorId=$data['exhibitor'][0]['exhibitorId'];
 $data2=array();
 $data3=array();
 $response=array();
-$response1=array();
-$response2=array();
 
 //select event title & description 
-@$selectExhibitorsQuery="select a.id,a.logoMediaId,a.primEmail,a.secEmail,a.primContact,a.secContact,a.about,b.companyName,c.address1,c.address2,c.pincode,c.maplink,d.`link` from exhibitorProfile a, exhibitor b, exhibitorAddress c,media d where a.createdBy=b.id and c.createdBy=b.id and  d.id=a.logoMediaId";
-@$selectExhibitors=mysql_query($selectExhibitorsQuery,$conn) or die(mysql_error());
-@$exhibitorRows=mysql_num_rows($selectExhibitors);
+@$ExhibitorsQuery="select a.id,a.logoMediaId,a.about,a.primEmail,a.secEmail,a.primContact,a.secContact,a.website,b.companyName,b.mobileNo,c.address1,c.address2,c.landmark,c.pincode,c.maplink,d.`link` from exhibitorProfile a, exhibitor b, exhibitorAddress c,media d where a.createdBy=b.id and c.createdBy=b.id and  d.id=a.logoMediaId and a.id='$exhibitorId'";
+@$selectExhibitorsDetails=mysql_query($ExhibitorsQuery,$conn) or die(mysql_error());
+@$exhibitorDetailsRows=mysql_num_rows($selectExhibitorsDetails);
 
-while($exhibitors=mysql_fetch_array($selectExhibitors))
+while($exhibitors=mysql_fetch_array($selectExhibitorsDetails))
 {
 
 		$response['id'] = $exhibitors['id'];
-		$response['companyName'] = $exhibitors['companyName'];
+		$response['about'] = $exhibitors['about'];
 		$response['primEmail'] = $exhibitors['primEmail'];
 		$response['secEmail'] = $exhibitors['secEmail'];
 		$response['primContact'] = $exhibitors['primContact'];
 		$response['secContact'] = $exhibitors['secContact'];
-		$response['image link'] = $exhibitors['link'];
-		$response['about'] = $exhibitors['about'];
+		$response['website'] = $exhibitors['website'];
+		$response['companyName'] = $exhibitors['companyName'];
+		$response['mobileNo'] = $exhibitors['mobileNo'];
 		$response['address1'] = $exhibitors['address1'];
 		$response['address2'] = $exhibitors['address2'];
+		$response['landmark'] = $exhibitors['landmark'];
 		$response['pincode'] = $exhibitors['pincode'];
 		$response['maplink'] = $exhibitors['maplink'];
-		
+		$response['link'] = $exhibitors['link'];
 		array_push($data,$response);	
 } 
 
